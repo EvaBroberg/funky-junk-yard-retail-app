d2a7beb6221256a3fc2549346fe6f9995f85a4ba
ieva broberg <ievabroberg@gmail.com>
2020-02-18 16:13:59 +0000

    updated menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const ClothesPage = () => (
+  <div>
+    <h1>CLOTHES PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/clothes' component={ClothesPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,31 +9,36 @@ class Directory extends React.Component {
         this.state = {
             sections: [
                 {
-                    title: 'Electronics/gaming',
-                    imageUrl: 'https://images.unsplash.com/photo-1556537570-231ead2a4009?ixlib=rb-1.2.1&auto=format&fit=crop&w=1266&q=80',
-                    id: 1
+                    title: 'Clothes',
+                    imageUrl: 'https://images.unsplash.com/photo-1579027846535-45a372a488b7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+                    id: 1,
+                    linkUrl: 'clothes'
                 },
                 {
-                    title: 'Home deco',
-                    imageUrl: 'https://images.unsplash.com/photo-1568639837178-c794256b9b98?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-                    id: 2
+                    title: 'Toys',
+                    imageUrl: 'https://images.unsplash.com/photo-1498940757830-82f7813bf178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
-                    title: 'Apparel',
-                    imageUrl: 'https://images.unsplash.com/photo-1504659913281-61817e6e2e9b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80',
-                    id: 3
+                    title: 'Beds',
+                    imageUrl: 'https://images.unsplash.com/photo-1519405474421-f6a5fae2db20?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+                    id: 3,
+                    linkUrl: ''
                 },
                 {
-                    title: 'Adults',
-                    imageUrl: 'https://images.unsplash.com/photo-1558493625-4de5c096ab21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+                    title: 'Dogs',
+                    imageUrl: 'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
                 },
                 {
-                    title: 'Children',
-                    imageUrl: 'https://images.unsplash.com/photo-1484725008675-4f26e4824ebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+                    title: 'Cats',
+                    imageUrl: 'https://images.unsplash.com/photo-1536500152107-01ab1422f932?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 }]
         }
     }
@@ -41,8 +46,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        < MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        < MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
